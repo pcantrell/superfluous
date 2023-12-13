d3e9303019fd201887cea3914d518c26d7a532ec
Paul Cantrell <cantrell@pobox.com>
2023-12-13 15:34:52 -0600

    Fancied up request logging

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,4 @@
+require 'ansi'
 require_relative 'engine'
 
 module Superfluous
@@ -40,6 +41,7 @@ module Superfluous
         exec((Pathname.new(__dir__) + "../bin/superfluous").realpath.to_s, *ARGV)
       end.start
 
+      override_web_server_logging!
       server = Adsf::Server.new(live: true, root: @engine.output_dir)
       %w[INT TERM].each do |s|
         Signal.trap(s) { server.stop }
@@ -72,6 +74,51 @@ module Superfluous
       File.write(trace_file, exception.full_message(highlight: false))
       puts "  detailed trace in: #{trace_file}"
       puts
+    end
+
+    # Turn back! Only horrifying monkey patches lie beyond this point.
+
+    def override_web_server_logging!
+      # Disable WEBrick’s logging completely; only use Rack logs
+      WEBrick::Config::HTTP[:AccessLog] = []
+
+      # Wrap Rack’s logging with our colorizer
+      ::Rack::CommonLogger.prepend(RackLogOverride)
+
+      # Hack to colorize adsf’s startup message with the site URL
+      ::Adsf::Server.prepend(AdsfLogOverride)
+    end
+
+    module RackLogOverride
+      LOG_MUTEX = Mutex.new
+
+      def log(env, status, *args)
+        LOG_MUTEX.synchronize do
+          if status >= 400
+            print ANSI.red
+            print ANSI.bold
+          elsif status == 304
+            print ANSI.dark
+          elsif status >= 300
+            print ANSI.yellow
+          end
+          super(env, status, *args)
+        ensure
+          print ANSI.clear
+        end
+      end
+    end
+
+    module AdsfLogOverride
+      def puts(*args)
+        if args[0] =~ /(View the site at )(.*)/
+          Kernel::puts ANSI.blue { $1 + ANSI.bold { $2 } }
+          Kernel::puts args[1..]
+          Kernel::puts
+        else
+          Kernel::puts(*args)
+        end
+      end
     end
   end
 end
